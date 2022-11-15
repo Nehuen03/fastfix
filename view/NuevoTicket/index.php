7c2c1a14355c1ac986de8e598cc0ee5dd94e6013
Nehuen03 <est.napoli.nehuen@latecnicalf.com.ar>
2022-11-15 19:16:19 -0300

    act

--- view/NuevoTicket/index.php
+++ view/NuevoTicket/index.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html>
     <?php require_once("../MainHead/head.php");?>
-	<title>AnderCode</>::Nuevo Ticket</title>
+	<title>Nuevo Ticket</title>
 </head>
 <body class="with-side-menu">
 
@@ -35,7 +35,7 @@
 
 			<div class="box-typical box-typical-padding">
 				<p>
-					Desde esta ventana podra generar nuevos tickets de HelpDesk.
+					Desde esta ventana podra generar nuevos tickets.
 				</p>
 
 				<h5 class="m-t-lg with-border">Ingresar Información</h5>
@@ -56,7 +56,7 @@
 							<fieldset class="form-group">
 								<label class="form-label semibold" for="exampleInput">Categoria</label>
 								<select id="cat_id" name="cat_id" class="form-control">
-
+									
 								</select>
 							</fieldset>
 						</div>
