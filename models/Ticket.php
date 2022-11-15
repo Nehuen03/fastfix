9f9df8003d1495695f95c07d9b6e4743bb0d0a5f
Nehuen03 <est.napoli.nehuen@latecnicalf.com.ar>
2022-11-15 19:30:50 -0300

    act

--- models/Ticket.php
+++ models/Ticket.php
@@ -4,7 +4,8 @@
         public function insert_ticket($usu_id,$cat_id,$tick_titulo,$tick_descrip){
             $conectar= parent::conexion();
             parent::set_names();
-            $sql="INSERT INTO tm_ticket (tick_id,usu_id,cat_id,tick_titulo,tick_descrip,tick_estado,fech_crea,usu_asig,fech_asig,est) VALUES (NULL,?,?,?,?,'Abierto',now(),NULL,NULL,'1');";
+            //$sql="INSERT INTO tm_ticket (tick_id,usu_id,cat_id,tick_titulo,tick_descrip,tick_estado,fech_crea,usu_asig,fech_asig,est) VALUES (NULL,?,?,?,?,'Abierto',now(),NULL,NULL,'1');";
+            $sql="INSERT INTO tm_ticket (tick_id,usu_id,cat_id,tick_titulo,tick_descrip,est) VALUES (NULL,?,?,?,?,'1');
             $sql=$conectar->prepare($sql);
             $sql->bindValue(1, $usu_id);
             $sql->bindValue(2, $cat_id);
